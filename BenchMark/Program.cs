7538797c780cb4c0fe50a6ea8d3966dce4caa98f
r.rejaba <raymond.c.rejaba@gmail.com>
2017-05-22 19:33:20 +0800

    Add project files.

---
+++ BenchMark/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenchMark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BenchMark challenge exam");
+
+            int[] array = new int[] { 3, 1, 4, 5, 2 };
+            array = SortList(array);
+
+            for (int i = 0; i < array.Length ; i++)
+            {
+                Console.Write(array[i]);
+            }
+
+            Console.ReadLine();
+        }
+
+
+        static int[] SortList(int[] array)
+        {
+            array = array.OrderBy(c => c).ToArray();
+            return array;
+        }
+    }
+}
